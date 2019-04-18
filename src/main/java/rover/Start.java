d590ecc6637a043599fd6d2b7c2051abcd0f2ed1
Anas El Methari <you@example.com>
2019-04-18 16:32:35 +0200

    Methoden und Variablen umbennant

--- src/main/java/rover/Start.java
+++ src/main/java/rover/Start.java
@@ -8,76 +8,76 @@ import java.util.Set;
 
 public class Start {
 
-	static Random r = new Random();
+	static Random random = new Random();
 	static LinkedHashMap<int[], String> mars;
 
-	public static void init() {
+	public static void befülleDenMars() {
 		mars = new LinkedHashMap<>();
-		int x = 80;
-		int y = 20;
-		int rx = x / 2;
-		int ry = y / 2;
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				int[] p = new int[] { i, j };
-				if (r.nextDouble() < 0.25 && !(rx == i && ry == j))
-					mars.put(p, "#");
+		int BREITE = 80;
+		int HÖHE = 20;
+		int HÄLFTE_X = BREITE / 2;
+		int HÄLFTE_Y = HÖHE / 2;
+		for (int i = 0; i < BREITE; i++) {
+			for (int j = 0; j < HÖHE; j++) {
+				int[] Koordinaten = new int[] { i, j };
+				if (random.nextDouble() < 0.25 && !(HÄLFTE_X == i && HÄLFTE_Y == j))
+					mars.put(Koordinaten, "#");
 			}
 		}
-		mars.put(new int[] { rx, ry }, "n");
+		mars.put(new int[] { HÄLFTE_X, HÄLFTE_Y }, "n");
 	}
 
-	public static int[] maximum(Set<int[]> set) {
-		int[] x = new int[2];
-		for (int[] e : set) {
-			if (e[0] > x[0])
-				x[0] = e[0];
-			if (e[1] > x[1])
-				x[1] = e[1];
+	public static int[] maxBreiteUndHöhe(Set<int[]> keys) {
+		int[] Koordinaten = new int[2];
+		for (int[] key : keys) {
+			if (key[0] > Koordinaten[0])
+				Koordinaten[0] = key[0];
+			if (key[1] > Koordinaten[1])
+				Koordinaten[1] = key[1];
 		}
-		return x;
+		return Koordinaten;
 	}
 
-	public static String get(Map<int[], String> kloetze, int[] p) {
+	public static String getZeichen(Map<int[], String> kloetze, int[] Koordinaten) {
 		Set<Entry<int[], String>> entrySet = kloetze.entrySet();
 		for (Entry<int[], String> entry : entrySet) {
-			if (entry.getKey()[0] == p[0] && entry.getKey()[1] == p[1])
+			if (entry.getKey()[0] == Koordinaten[0] && entry.getKey()[1] == Koordinaten[1])
 				return entry.getValue();
 		}
 		return null;
 	}
 
-	public static void out() {
+	public static void zeichneMap() {
 		// Set<int[]> keySet = mars.keySet();
 		// for (int[] e : keySet) {
 		// if (e[0] == 39 && e[1] == 10)
 		// System.err.println(mars.get(e) + " " + e.hashCode());
 		// }
 
-		int[] max = maximum(mars.keySet());
-		for (int j = 0; j < max[1]; j++) {
-			for (int i = 0; i < max[0]; i++) {
+		int[] Koordinaten = maxBreiteUndHöhe(mars.keySet());
+		for (int j = 0; j < Koordinaten[1]; j++) {
+			for (int i = 0; i < Koordinaten[0]; i++) {
 				// System.out.println(i + "," + j + ": " + get(mars, new int[] { i, j }));
 
-				if (get(mars, new int[] { i, j }) == null) {
+				if (getZeichen(mars, new int[] { i, j }) == null) {
 					System.out.print(" ");
 					continue;
 				}
-				if (get(mars, new int[] { i, j }).equals("#"))
+				if (getZeichen(mars, new int[] { i, j }).equals("#"))
 					System.out.print("#");
-				else if (get(mars, new int[] { i, j }).equals("n"))
+				else if (getZeichen(mars, new int[] { i, j }).equals("n"))
 					System.out.print("^");
-				else if (get(mars, new int[] { i, j }).equals("s"))
+				else if (getZeichen(mars, new int[] { i, j }).equals("s"))
 					System.out.print("V");
-				else if (get(mars, new int[] { i, j }).equals("e"))
+				else if (getZeichen(mars, new int[] { i, j }).equals("e"))
 					System.out.print(">");
-				if (get(mars, new int[] { i, j }).equals("w"))
+				if (getZeichen(mars, new int[] { i, j }).equals("w"))
 					System.out.print("<");
 
 			}
 			System.out.println();
 		}
-		for (int i = 0; i < max[0]; i++) {
+		for (int i = 0; i < Koordinaten[0]; i++) {
 			System.out.print("=");
 		}
 		System.out.println();
@@ -85,60 +85,61 @@ public class Start {
 
 	public static void main(String[] args) {
 
-		if (args.length > 1) {
-			long seed = Long.parseLong(args[1]);
-			r.setSeed(seed);
+		//if (args.length > 1) {
+		//	long seed = Long.parseLong(args[1]);
+		//	random.setSeed(seed);
 			// System.out.println("Seed: " + seed);
-		}
-		init();
-		String pg = args[0];
-		out();
-		for (int i = 0; i < pg.length(); i++) {
-			make(pg.charAt(i));
-			out();
+		//}
+		
+		befülleDenMars();
+		String eingabe = args[0];
+		zeichneMap();
+		for (int i = 0; i < eingabe.length(); i++) {
+			bewegeDenRover(eingabe.charAt(i));
+			zeichneMap();
 		}
 	}
 
-	public static void make(char c) {
-		if (c == 'f') {
+	public static void bewegeDenRover(char bewegung) {
+		if (bewegung == 'f') {
 			int[] p = findeRover();
-			if (get(mars, p).equals("n"))
+			if (getZeichen(mars, p).equals("n"))
 				p[1]--;
-			else if (get(mars, p).equals("s"))
+			else if (getZeichen(mars, p).equals("s"))
 				p[1]++;
-			else if (get(mars, p).equals("e"))
+			else if (getZeichen(mars, p).equals("e"))
 				p[0]++;
-			else if (get(mars, p).equals("w"))
+			else if (getZeichen(mars, p).equals("w"))
 				p[0]--;
-		} else if (c == 'b') {
+		} else if (bewegung == 'b') {
 			int[] p = findeRover();
-			if (get(mars, p).equals("s"))
+			if (getZeichen(mars, p).equals("s"))
 				p[1]--;
-			else if (get(mars, p).equals("n"))
+			else if (getZeichen(mars, p).equals("n"))
 				p[1]++;
-			else if (get(mars, p).equals("w"))
+			else if (getZeichen(mars, p).equals("w"))
 				p[0]++;
-			else if (get(mars, p).equals("e"))
+			else if (getZeichen(mars, p).equals("e"))
 				p[0]--;
-		} else if (c == 'l') {
+		} else if (bewegung == 'l') {
 			int[] p = findeRover();
-			if (get(mars, p).equals("n"))
+			if (getZeichen(mars, p).equals("n"))
 				mars.put(p, "w");
-			else if (get(mars, p).equals("s"))
+			else if (getZeichen(mars, p).equals("s"))
 				mars.put(p, "e");
-			else if (get(mars, p).equals("e"))
+			else if (getZeichen(mars, p).equals("e"))
 				mars.put(p, "n");
-			else if (get(mars, p).equals("w"))
+			else if (getZeichen(mars, p).equals("w"))
 				mars.put(p, "s");
-		} else if (c == 'r') {
+		} else if (bewegung == 'r') {
 			int[] p = findeRover();
-			if (get(mars, p).equals("w"))
+			if (getZeichen(mars, p).equals("w"))
 				mars.put(p, "n");
-			else if (get(mars, p).equals("e"))
+			else if (getZeichen(mars, p).equals("e"))
 				mars.put(p, "s");
-			else if (get(mars, p).equals("n"))
+			else if (getZeichen(mars, p).equals("n"))
 				mars.put(p, "e");
-			else if (get(mars, p).equals("s"))
+			else if (getZeichen(mars, p).equals("s"))
 				mars.put(p, "w");
 		}
 
